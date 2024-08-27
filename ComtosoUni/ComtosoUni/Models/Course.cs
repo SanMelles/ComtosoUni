c1b3ddd0e97bbaec4c4f0fa183621729f4c55732
sandermelles@gmail.com <sandermelles@gmail.com>
2024-08-27 09:51:36 +0300

    commit

---
+++ ComtosoUni/ComtosoUni/Models/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace ComtosoUni.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
