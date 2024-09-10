ae0a1f48e08ef3d9c93f2cc66468e0c08e406813
sandermelles@gmail.com <sandermelles@gmail.com>
2024-09-10 11:42:50 +0300

    Detailsi fix, Pagination.

--- ComtosoUni/ComtosoUni/Controllers/StudentsController.cs
+++ ComtosoUni/ComtosoUni/Controllers/StudentsController.cs
@@ -17,15 +17,60 @@ namespace ComtosoUni.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(
+            string sortOrder, 
+            string currentFilter,
+            string searchString,
+            int? pageNumber
+            )
         {
-            var result = await _context
-                .Students.ToListAsync();
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["ameSortParam"] = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+			ViewData["DateSortParam"] = sortOrder == "Date" ? "date_desc" : "Date";
 
-            return View(result);
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+            var students = from s in _context.Students
+                           select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.Contains(searchString)
+                                || s.FirstMidName.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(s => s.LastName); 
+                    break;
+
+                case "Date":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+
+                case "date_desc":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+
+                default:
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+            }
+
+            int pageSize = 3;
+            return View(await PaginatedList<Student>.CreateAsync(students.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
 
-        public async Task<ActionResult> Details(int? id)
+        public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
